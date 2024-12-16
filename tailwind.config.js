3ccef3befe70ab227106cc0839de0d6c70e6de60
Fabien Le Frapper <contact@fabienlefrapper.me>
2024-12-16 13:56:25 +0100

    Add transition to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,22 @@ module.exports = {
     "./static/**/*.js",      // JavaScript files in the static folder
   ],
   theme: {
-    extend: {},
+    extend: {
+          keyframes: {
+            rainbow: {
+              '0%': { color: 'red' },
+              '16.7%': { color: 'orange' },
+              '33.4%': { color: 'yellow' },
+              '50%': { color: 'green' },
+              '66.7%': { color: 'blue' },
+              '83.4%': { color: 'indigo' },
+              '100%': { color: 'violet' },
+            },
+          },
+          animation: {
+            rainbow: 'rainbow 1s linear infinite',
+          },
+        },
   },
   plugins: [],
 }
